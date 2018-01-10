54503565213efc51b7feed7d8ebaedbdc744f2f6
Your Name <you@example.com>
2018-01-10 23:09:21 +0800

    group

--- kaggle/Kaggle_Titanic/group_by.py
+++ kaggle/Kaggle_Titanic/group_by.py
@@ -5,14 +5,38 @@ from pandas import Series,DataFrame
 
 import matplotlib.pyplot as plt
 
+#pandas.DataFrame.plot
+
+
+
+
 
 data_train = pd.read_csv("train.csv")
 data_train.columns
 data_train[data_train.Cabin.notnull()]['Survived'].value_counts()
 
-g=data_train.groupby(['Cabin']).sum()
+sub=['Cabin','Survived']
+data=data_train[sub]
+
+g=data.groupby(['Cabin']).sum()
+
+print ('================kde')
+
+print (type(g))
+print ('=========================kde')
 g.plot(kind='kde')
 g.plot(kind='bar')
+g.plot(kind='line')
+g.plot(kind='hist')
+g.plot(kind='box')
+g.plot(kind='area')
+#g.plot(kind='pie')
+
+#g.plot(kind='scatter')
+
+
+
+
 plt.show()
 
 
