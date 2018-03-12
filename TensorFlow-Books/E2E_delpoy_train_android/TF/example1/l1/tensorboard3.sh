8742f712e370294824b500f0d590c8e06f8c5011
Your Name <you@example.com>
2018-03-12 22:55:40 +0800

    ¨graph_write¨

---
+++ TensorFlow-Books/E2E_delpoy_train_android/TF/example1/l1/tensorboard3.sh
@@ -0,0 +1,5 @@
+python tools/import_pb_to_tensorboard.py --model_dir frozen_tfdroid.pb --log_dir ./tmp-pb-froze/tensorboard
+python tools/import_pb_to_tensorboard.py --model_dir optimized_tfdroid.pb --log_dir ./tmp-pb-optmized/tensorboard
+
+
+
