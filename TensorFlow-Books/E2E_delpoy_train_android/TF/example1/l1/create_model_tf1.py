03500c92edd03f731a7074112e24ffe877c20a08
haijunz <haijunz@sina.com>
2018-03-12 17:33:06 +0800

    git result

---
+++ TensorFlow-Books/E2E_delpoy_train_android/TF/example1/l1/create_model_tf1.py
@@ -0,0 +1,26 @@
+# Create a simple TF Graph 
+# By Omid Alemi - Jan 2017
+# Works with TF r1.0
+
+import tensorflow as tf
+import os
+I = tf.placeholder(tf.float32, shape=[None,3], name='I') # input
+W = tf.Variable(tf.zeros(shape=[3,2]), dtype=tf.float32, name='W') # weights
+b = tf.Variable(tf.zeros(shape=[2]), dtype=tf.float32, name='b') # biases
+O = tf.nn.relu(tf.matmul(I, W) + b, name='O') # activation / output
+
+saver = tf.train.Saver()
+init_op = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	sess.run(init_op)
+  # save the graph 
+	tf.train.write_graph(sess.graph_def, './', 'tfdroid.pbtxt')  
+
+  # normally you would do some training here
+  # but fornow we will just assign something to W
+	sess.run(tf.assign(W, [[1, 2],[4,5],[7,8]]))
+  	sess.run(tf.assign(b, [1,1]))
+
+  #save a checkpoint file, which will store the above assignment  
+  	saver.save(sess, './tfdroid.ckpt')
