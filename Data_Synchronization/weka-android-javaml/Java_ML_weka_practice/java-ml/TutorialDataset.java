568ba8f495095d66f2bff4d1a560744e0ee43f2f
Your Name <you@example.com>
2018-04-01 18:01:56 +0800

    javaml-we-need-KNN-myself-cnn-example

---
+++ Data_Synchronization/weka-android-javaml/Java_ML_weka_practice/java-ml/TutorialDataset.java
@@ -0,0 +1,56 @@
+
+import java.util.SortedSet;
+
+import net.sf.javaml.core.Dataset;
+import net.sf.javaml.core.DefaultDataset;
+import net.sf.javaml.core.Instance;
+import net.sf.javaml.core.InstanceTools;
+
+import org.junit.Assert;
+
+/**
+ * This tutorial show how to create a {@link net.sf.javaml.core.Dataset} from a
+ * collection of instances. This tutorial assumes you know how to create an
+ * {@link net.sf.javaml.core.Instance}. To create instances for this tutorial
+ * we will use a method from {@link net.sf.javaml.core.InstanceTools} to create
+ * random instances.
+ * 
+ * In this tutorial we will create a number of instances and group them in a
+ * data set.
+ * 
+ * Basically a data set is a collection of instances.
+ * 
+ * {@jmlSource}
+ * 
+ * @see CreatingAnInstance
+ * @see net.sf.javaml.core.Instance
+ * @see net.sf.javaml.core.Dataset
+ * @see net.sf.javaml.core.DefaultDataset
+ * @see net.sf.javaml.core.InstanceTools
+ * 
+ * @version 0.1.4
+ * 
+ * @author Thomas Abeel
+ * 
+ */
+public class TutorialDataset {
+
+    /**
+     * Create a data set and put some instances in it.
+     */
+   public static void main(String[]args){
+        Dataset data = new DefaultDataset();
+        for (int i = 0; i < 10; i++) {
+            Instance tmpInstance = InstanceTools.randomInstance(25);
+            data.add(tmpInstance);
+        }
+        /* Retrieve all class values that are ever used in the data set */
+        SortedSet<Object> classValues = data.classes();
+        
+
+        Assert.assertEquals(data.noAttributes(), 25);
+        Assert.assertEquals(data.size(), 10);
+        Assert.assertEquals(classValues.size(), 0);
+
+    }
+}
---
+++ Data_Synchronization/weka-android-javaml/Java_ML_weka_practice/java-ml/my.java
@@ -0,0 +1,28 @@
+import weka.core.Attribute;
+import weka.core.Instance;
+import weka.core.Instances;
+
+/**
+ * Generates a little ARFF file with different attribute types.
+ *
+ * @author FracPete
+ */
+public class AttTest {
+  public static void main(String[] args) throws Exception {
+    
+
+Instance instance = Weka::Core::DenseInstance.new(11);
+
+
+
+
+
+
+
+
+
+
+
+//System.out.println(data);
+  }
+}
